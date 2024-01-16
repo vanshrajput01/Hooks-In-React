f90b05130bdbbd2f4e959f1626969875e2491954
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-16 12:26:55 +0530

    Project complete

--- Hooks-In-React/src/Images.jsx
+++ Hooks-In-React/src/Images.jsx
@@ -1,5 +1,8 @@
 function Images(props){
-    
+    return   <img src={props.src} alt="Enter Valid Url in input Box!!" />
+
+  
+
 
 }
 
--- Hooks-In-React/src/ProjectInUseSateHook.jsx
+++ Hooks-In-React/src/ProjectInUseSateHook.jsx
@@ -1,3 +1,4 @@
+import Images from "./Images";
 import { useState } from "react";
 
 function ProjectInUseSateHook() {
@@ -5,17 +6,30 @@ function ProjectInUseSateHook() {
     const inputHandler = (e) => {
         setInput(e.target.value)
     }
+    const seeImage = (e) =>{
+        e.preventDefault()
+        const imageContainer = document.querySelector("#image-container")
+        imageContainer.children[0].setAttribute("src",input)
+                
+    }
     return (
 
         <>
             <h2>Project In Use state Hook in React</h2>
-            <form>
+            <form onSubmit={seeImage}>
                 <input type="text" placeholder="Enter Url Only" required onChange={inputHandler} />
                 <br></br>
                 <input type="submit" value="See image" />
                 <input type="reset" value="Reset Url" />
 
             </form>
+            <div id="image-container">
+                <Images />
+            </div>
+           
+            
+
+
 
         </>
     )
