1ad8eab90056666d34375c49dbfba7c70a7c9698
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-16 12:00:26 +0530

    some update in Registration.jsx

--- Hooks-In-React/src/RegistrationFrom.jsx
+++ Hooks-In-React/src/RegistrationFrom.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 function RegistrationForm(){
+    const [input,setInput] = useState("")
+
+    const inputHandler = (e) =>{
+        setInput(e.target.value)
+    }
+
+    const seeData = (e) =>{
+        e.preventDefault()
+        const p = document.querySelector("#data");
+        p.innerHTML = `Your enter data is : - ${input}`
+
+
+    }
     return(
         <>
         <h1>Registration Form</h1>
+        <form action="">
+            <input type="text"  placeholder="Enter Any thing" onChange={inputHandler}/>
+            <button onClick={seeData}>See data</button>
+            <input type="reset" />
+        </form>
+        <p id="data"></p>
+
+        
         </>
     )
 
