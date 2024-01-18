548806b99914dda0564f4c79cebf9d96699c7ec5
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-18 16:00:11 +0530

    Example of use Effect Hooks in React

--- Hooks-In-React/src/Project03OnHooks.jsx
+++ Hooks-In-React/src/Project03OnHooks.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 function Project03OnHooks(){
+    const [size,setSize] = useState(window.innerWidth)
+    useEffect(()=>{
+        window.addEventListener("resize",()=>{
+            setSize(window.innerWidth)
+        })
+
+        // Clean up function that is not maintain privious state data
+        
+        return ()=>{
+            window.removeEventListener("resize",()=>{
+                setSize(window.innerWidth)
+
+            })
+        }
+    })
+
+
     return (
     <>
     <h1>Project 03 On Hooks</h1>
+    <h1>Your Current Window Screen Size is <span>{size}</span></h1>
+
     </>
     )
 
